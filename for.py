dcca1fad3ef86738dcfa16686ce9d71a0d41d5b0
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-19 21:52:23 +0700

    komentar

---
+++ for.py
@@ -0,0 +1,28 @@
+benda = ["buku","pensil","meja"]
+for x in (benda):
+    print(x)
+    print(x + "kursi")
+    
+
+print("="*40)
+
+tinggi_badan = [145,150,160,170,180]
+for i in(0,len(tinggi_badan)):
+    print(tinggi_badan)
+
+print("="*40)
+
+for buah in "mangga":
+    if buah == "g":
+        print(buah)
+        break
+print("end")
+
+print("="*40)
+
+for item in "akua":
+    if item == "u":
+        print(item)
+        continue
+print("end")
+
